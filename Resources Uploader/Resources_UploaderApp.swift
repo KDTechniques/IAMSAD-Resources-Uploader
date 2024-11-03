aac7ffc50def0f9dbfde1af6c4f79c67d1966526
Kavinda Dilshan <kdtechniques@gmail.com>
2024-11-04 01:28:54 +0530

    Initial Commit

---
+++ Resources Uploader/Resources_UploaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resources_UploaderApp.swift
+//  Resources Uploader
+//
+//  Created by Mr. Kavinda Dilshan on 2024-11-04.
+//
+
+import SwiftUI
+
+@main
+struct Resources_UploaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
